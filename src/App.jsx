8084039e52d83362b3146b449d8514dcc4ea800b
Tom Lorius <tomlorius.mail@gmail.com>
2023-12-11 17:53:34 +0100

    added css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       <Footer />
+      <h2 id="home">Home Page</h2>
+      <h2 id="about">About Page</h2>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <aside>
+      <a href="#home">Home Page</a>
+      <a href="#about">About</a>
+    </aside>
+  );
+};
+
+export default Sidebar;
