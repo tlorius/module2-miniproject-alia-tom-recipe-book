5e6ddb116b3432ba519df9435dd6aca5b843ca77
Tom Lorius <tomlorius.mail@gmail.com>
2023-12-11 17:24:35 +0100

    moved file structure, created first components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/tlorius/module2-miniproject-alia-tom-recipe-book">
+        GitHub Repository
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>
+        <img src="" alt="Recipe Book Logo" />
+        Recipe Book
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
