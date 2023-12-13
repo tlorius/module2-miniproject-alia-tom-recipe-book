4d0b22af81c922843134697ab39564ac46d2dc2c
Alia Dossani <dossanialia1408@gmail.com>
2023-12-13 22:23:48 +0100

    added the 404 page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,10 @@
+import classes from "../styles/About.module.css"
 const About = ({ prop1 }) => {
-  return <h1>About Page Content</h1>;
+  return (
+    <div className={classes.aboutCtn}>
+      <h4>Welcome to the Recipe Book React App, where culinary creativity meets the digital world! This app is your go-to destination for a treasure trove of delicious recipes, beautifully presented with mouthwatering images. Whether you are a seasoned chef or a kitchen novice, our Recipe Book is designed to inspire and simplify your cooking journey.</h4>
+    </div>
+  )
 };
 
 export default About;
--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,19 @@
-const Error404 = ({ prop1 }) => {
-  return <h1>404 - no page found</h1>;
+import classes from "../styles/Error404.module.css"
+import { Link } from "react-router-dom";
+
+const Error404 = () => {
+  return (
+    <center className={classes.page404}>
+      <h1>404</h1>
+      <h3>Page not found</h3>
+      <h4>The requested URL was not found on this server</h4>
+      <img className={classes.fourZeroFourImage} src="https://images01.nicepagecdn.com/c461c07a441a5d220e8feb1a/6df13c16b8e65c1290bec8c0/yyy.jpg" alt="404 image" />
+      <div className={classes.buttonContainer}>
+        <Link to='/' className={`${classes.primaryBtn} ${classes.btn}`}>HOME</Link>
+        <Link to='/about' className={`${classes.secondaryBtn} ${classes.btn}`}>ABOUT US</Link>
+      </div>
+    </center>
+  )
 };
 
 export default Error404;
