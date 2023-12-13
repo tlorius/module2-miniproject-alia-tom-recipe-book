562d67b4c3329d00b5ce5336b2d0d8a5aa56c084
Tom Lorius <tomlorius.mail@gmail.com>
2023-12-13 17:18:14 +0100

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,39 +2,21 @@ import classes from "../styles/Navbar.module.css";
 import recipeBookLogo from "../assets/recipe_book_logo.png";
 import gitHubLogo from "../assets/github-mark-white.png";
 import menuIcon from "../assets/menu_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={classes.navbar}>
-      <img
-        className={classes.navbarImage}
-        src={recipeBookLogo}
-        alt="Recipe Book Logo"
-      />
-      <h1>Recipe Book</h1>
-      <div>
-        <a
-          className={classes.containerLinks}
-          href="https://github.com/aliadossani"
-        >
+      <h1>
+        <Link to="/">
           <img
-            className={classes.gitHubImg}
-            src={gitHubLogo}
-            alt="GitHub Logo"
-          />{" "}
-          Alia Dossani
-        </a>
-        <p> </p>
-        <a className={classes.containerLinks} href="https://github.com/tlorius">
-          <img
-            className={classes.gitHubImg}
-            src={gitHubLogo}
-            alt="GitHub Logo"
-          />{" "}
-          Tom Lorius
-        </a>
-      </div>
-      <img className={classes.navbarImage} src={menuIcon} alt="Menu Icon" />
+            className={classes.navbarImage}
+            src={recipeBookLogo}
+            alt="Recipe Book Logo"
+          />
+          Recipe Book
+        </Link>
+      </h1>
     </nav>
   );
 };
