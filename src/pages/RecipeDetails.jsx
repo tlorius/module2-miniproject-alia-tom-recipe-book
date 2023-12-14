bf2e2da7029e16d1e5a4fe1fb8917f4c87063432
Tom Lorius <tomlorius.mail@gmail.com>
2023-12-14 17:51:42 +0100

    added basic styling to recipedetailspage

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import classes from "../styles/RecipeDetails.module.css";
 import { v4 } from "uuid";
 
@@ -10,42 +10,56 @@ const RecipeDetails = ({ recipeList }) => {
   );
   return (
     <div className={classes.mainContainer}>
-      <h2>{currentRecipeDetails.name}</h2>
       <img
         className={classes.recipeImg}
         src={currentRecipeDetails.image}
         alt={`${currentRecipeDetails.name}`}
       />
-      <p>Description: {currentRecipeDetails.description}</p>
-      <p>Duration: {currentRecipeDetails.duration}</p>
-      <p>Servings: {currentRecipeDetails.servings}</p>
-      <div>
-        <h4>Ingredients:</h4>
-        <ul>
-          {currentRecipeDetails.ingredients.map((currentIngredient) => (
-            <li key={v4()}>
-              {currentIngredient.amount} {currentIngredient.unit}{" "}
-              {currentIngredient.ingredient}
-            </li>
-          ))}
-        </ul>
+      <h1 className={classes.title}>{currentRecipeDetails.name}</h1>
+      <div className={classes.cntInformation}>
+        <div className={classes.mainInformation}>
+          <p>Description: {currentRecipeDetails.description}</p>
+          <p>Duration: {currentRecipeDetails.duration}</p>
+          <p>Servings: {currentRecipeDetails.servings}</p>
+        </div>
+        <div className={classes.nutritionalInformation}>
+          <h4>Nutritional Information</h4>
+          <p>Calories: {currentRecipeDetails.nutrionalInformation.calories}</p>
+          <p>Fat: {currentRecipeDetails.nutrionalInformation.fat} g</p>
+          <p>Carbs: {currentRecipeDetails.nutrionalInformation.carbs} g</p>
+          <p>Protein: {currentRecipeDetails.nutrionalInformation.protein} g</p>
+        </div>
       </div>
+
+      <div className={classes.cntCooking}>
+        <div>
+          <h4>Ingredients:</h4>
+          <ul className={classes.centerList}>
+            {currentRecipeDetails.ingredients.map((currentIngredient) => (
+              <li key={v4()}>
+                {currentIngredient.amount} {currentIngredient.unit}{" "}
+                {currentIngredient.ingredient}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h4>Steps to Cook:</h4>
+          <ol className={classes.centerList}>
+            {currentRecipeDetails.stepsToCook.map((currentStep) => (
+              <li key={v4()}>{currentStep}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
+
       {/* should probably create its own component from the steps to cook*/}
-      <div>
-        <h4>Steps to Cook:</h4>
-        <ol>
-          {currentRecipeDetails.stepsToCook.map((currentStep) => (
-            <li key={v4()}>{currentStep}</li>
-          ))}
-        </ol>
-      </div>
-      <div>
-        <h4>Nutritional Information:</h4>
-        <p>Calories: {currentRecipeDetails.nutrionalInformation.calories}</p>
-        <p>Fat: {currentRecipeDetails.nutrionalInformation.fat}</p>
-        <p>Carbs: {currentRecipeDetails.nutrionalInformation.carbs}</p>
-        <p>Protein: {currentRecipeDetails.nutrionalInformation.protein}</p>
-      </div>
+
+      <Link to="/">
+        <button className={classes.backBtn} type="button">
+          BACK
+        </button>
+      </Link>
     </div>
   );
 };
